5143ff0ee00be116860592222ddd6dba8d855af6
rtalmage <raymondtalmage@gmail.com>
2020-07-26 14:46:18 -0700

    lab done

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get("/earth", function(req, res){
     res.render("earth.html");
 });
 
+app.get("/mars", function(req, res){
+    res.render("mars.html");
+});
+
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
